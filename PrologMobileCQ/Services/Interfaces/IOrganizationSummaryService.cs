025e7733895515d74d78bc1e9a9ee847a9ea91ad
Ryan Battistone <gugraves@gmail.com>
2021-01-24 20:00:24 -0800

    Declared in Interface for unit test to access

--- PrologMobileCQ/Services/Interfaces/IOrganizationSummaryService.cs
+++ PrologMobileCQ/Services/Interfaces/IOrganizationSummaryService.cs
@@ -10,5 +10,9 @@ namespace PrologMobileCQ.Services.Interfaces
     {
         Task<IList<T>> DeserializeDataIntoListOfClass<T>(string endpoint);
         Task<IList<SummaryForEachOrganizationDto>> ReturnASummaryForEachOrganization();
+        IList<SummaryForEachOrganizationDto> CreateSummaryForEachOrganization(IList<OrganizationDto> organizationList, IList<RegisteredUserDto> regUserList, IList<PhoneInformationDto> phoneInformationList)
+        string GenerateBlacklistTotal(string organizationID, IList<RegisteredUserDto> regUserList, IList<PhoneInformationDto> phoneInformationList);
+        string GenerateTotalCount(string organizationID, IList<RegisteredUserDto> regUserList, IList<PhoneInformationDto> phoneInformationList);
+        List<UserSummaryDto> GenerateUsers(string organizationID, IList<RegisteredUserDto> regUserList, IList<PhoneInformationDto> phoneInformationList);
     }
 }
