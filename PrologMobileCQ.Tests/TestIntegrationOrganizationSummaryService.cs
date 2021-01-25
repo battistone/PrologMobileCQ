927c71a63af91a0595d7062a86c472ff24022fee
Ryan Battistone <gugraves@gmail.com>
2021-01-24 19:58:23 -0800

    Refactored existing unit tests to use iloggerfactory.

--- PrologMobileCQ.Tests/TestIntegrationOrganizationSummaryService.cs
+++ PrologMobileCQ.Tests/TestIntegrationOrganizationSummaryService.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using PrologMobileCQ.Models.DTOs;
 using PrologMobileCQ.Services.Classes;
@@ -13,61 +15,86 @@ namespace PrologMobileCQ.Tests
         public async Task IsReturningAllOrganizationsWithAuthorizedFundsInTheSystem()
         {
             // Arrange
-            const int EMPTY = 0;
-            var oss = new OrganizationSummaryService();
-            string authFundEP = CommonEndpointStrings.AuthFundEndpoint;
-            // Act
-            IList<OrganizationDto> organizations = await oss.DeserializeDataIntoListOfClass<OrganizationDto>(authFundEP);
-            // Assert
-            Assert.IsTrue(organizations != null);
+            var services = new ServiceCollection()
+            .AddLogging(config => config.AddConsole())      // can add any logger(s)
+            .BuildServiceProvider();
+            using (var loggerFactory = services.GetRequiredService<ILoggerFactory>())
+            {
+                const int EMPTY = 0;
+                var oss = new OrganizationSummaryService(loggerFactory.CreateLogger<OrganizationSummaryService>());
+                string authFundEP = CommonEndpointStrings.AuthFundEndpoint;
+                // Act
+                IList<OrganizationDto> organizations = await oss.DeserializeDataIntoListOfClass<OrganizationDto>(authFundEP);
+                // Assert
+                Assert.IsTrue(organizations != null);
 
-            long dataCount = organizations.Count;
-            Assert.IsTrue(dataCount > EMPTY);
+                long dataCount = organizations.Count;
+                Assert.IsTrue(dataCount > EMPTY);
+            }
+
         }
         [Test]
         public async Task IsReturningRegisteredUsersForTheProvidedOrganization()
         {
             // Arrange
-            const int EMPTY = 0;
-            var oss = new OrganizationSummaryService();
-            string regUserEP = CommonEndpointStrings.RegisteredUserEndpoint;
-            // Act
-            IList<RegisteredUserDto> users = await oss.DeserializeDataIntoListOfClass<RegisteredUserDto>(regUserEP);
-            // Assert
-            Assert.IsTrue(users != null);
+            var services = new ServiceCollection()
+            .AddLogging(config => config.AddConsole())      // can add any logger(s)
+            .BuildServiceProvider();
+            using (var loggerFactory = services.GetRequiredService<ILoggerFactory>())
+            {
+                const int EMPTY = 0;
+                var oss = new OrganizationSummaryService(loggerFactory.CreateLogger<OrganizationSummaryService>());
+                string regUserEP = CommonEndpointStrings.RegisteredUserEndpoint;
+                // Act
+                IList<RegisteredUserDto> users = await oss.DeserializeDataIntoListOfClass<RegisteredUserDto>(regUserEP);
+                // Assert
+                Assert.IsTrue(users != null);
 
-            long dataCount = users.Count;
-            Assert.IsTrue(dataCount > EMPTY);
+                long dataCount = users.Count;
+                Assert.IsTrue(dataCount > EMPTY);
+            }
         }
         [Test]
         public async Task IsReturningPhoneInformationForTheProvidedOrganization()
         {
             // Arrange
-            const int EMPTY = 0;
-            var oss = new OrganizationSummaryService();
-            string phoneNumEP = CommonEndpointStrings.PhoneInformationEndpoint;
-            // Act
-            IList<PhoneInformationDto> phoneInfo = await oss.DeserializeDataIntoListOfClass<PhoneInformationDto>(phoneNumEP);
-            // Assert
-            Assert.IsTrue(phoneInfo != null);
+            var services = new ServiceCollection()
+            .AddLogging(config => config.AddConsole())      // can add any logger(s)
+            .BuildServiceProvider();
+            using (var loggerFactory = services.GetRequiredService<ILoggerFactory>())
+            {
+                const int EMPTY = 0;
+                var oss = new OrganizationSummaryService(loggerFactory.CreateLogger<OrganizationSummaryService>());
+                string phoneNumEP = CommonEndpointStrings.PhoneInformationEndpoint;
+                // Act
+                IList<PhoneInformationDto> phoneInfo = await oss.DeserializeDataIntoListOfClass<PhoneInformationDto>(phoneNumEP);
+                // Assert
+                Assert.IsTrue(phoneInfo != null);
 
-            long dataCount = phoneInfo.Count;
-            Assert.IsTrue(dataCount > EMPTY);
+                long dataCount = phoneInfo.Count;
+                Assert.IsTrue(dataCount > EMPTY);
+            }
         }
         [Test]
         public async Task AssertEndpointWorks()
         {
             // Arrange
-            const int EMPTY = 0;
-            var oss = new OrganizationSummaryService();
-            string phoneNumEP = CommonEndpointStrings.PhoneInformationEndpoint;
-            // Act
-            IList<PhoneInformationDto> phoneInfo = await oss.DeserializeDataIntoListOfClass<PhoneInformationDto>(phoneNumEP);
-            // Assert
-            Assert.IsTrue(phoneInfo != null);
+            var services = new ServiceCollection()
+            .AddLogging(config => config.AddConsole())      // can add any logger(s)
+            .BuildServiceProvider();
+            using (var loggerFactory = services.GetRequiredService<ILoggerFactory>())
+            {
+                const int EMPTY = 0;
+                var oss = new OrganizationSummaryService(loggerFactory.CreateLogger<OrganizationSummaryService>());
+                string phoneNumEP = CommonEndpointStrings.PhoneInformationEndpoint;
+                // Act
+                IList<PhoneInformationDto> phoneInfo = await oss.DeserializeDataIntoListOfClass<PhoneInformationDto>(phoneNumEP);
+                // Assert
+                Assert.IsTrue(phoneInfo != null);
 
-            long dataCount = phoneInfo.Count;
-            Assert.IsTrue(dataCount > EMPTY);
+                long dataCount = phoneInfo.Count;
+                Assert.IsTrue(dataCount > EMPTY);
+            }
         }
     }
 }
